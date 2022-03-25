8cc083c156e3dfa21d436097897edb499a143697
ferdeolazabal <ferdeolazabal@gmail.com>
2022-03-25 07:51:20 -0300

    2

--- db/config.ts
+++ db/config.ts
@@ -1,11 +1,13 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
+dotenv.config();
 
 import { PropertyValue } from '../models/PropertyValue';
 import { PropertyCategory } from '../models/PropertyCategory';
 import { Vehicle } from '../models/Vehicle';
 import { VehicleProperty } from '../models/VehicleProperty';
-dotenv.config();
+
+const { DATABASE_URL } = process.env;
 
 const dbName     = process.env.DB_NAME as string
 const dbUser     = process.env.DB_USER as string
@@ -13,10 +15,11 @@ const dbPassword = process.env.DB_PASSWORD as string
 const dbHost     = process.env.DB_HOST as string
 
 // export const sequelizeConnection = new Sequelize( dbName, dbUser , dbPassword, {
-export const sequelizeConnection = new Sequelize( `${ process.env.DB_DATABASE_URL }`, {
+export const sequelizeConnection = new Sequelize( `${DATABASE_URL}` , {
     host: dbHost,
     dialect: 'postgres',
     logging: false,
+    native: false,
     models: [PropertyValue, PropertyCategory, Vehicle, VehicleProperty],
     dialectOptions: { ssl: { rejectUnauthorized: false } },
 });
