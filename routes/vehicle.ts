cbb600c7ed672f1d539ba6644d0802e705304d36
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-25 18:06:36 -0300

    Add new method
    - getVehicleValuesByVehicleId added
    -Add to vehicles route

--- routes/vehicle.ts
+++ routes/vehicle.ts
@@ -1,18 +1,20 @@
-import { Router } from 'express';
-import { 
-        getVehicles, 
-        getVehicle, 
-        postVehicle, 
-        putVehicle, 
-        deleteVehicle 
-        } from '../controllers/Vehicle';
+import { Router } from "express";
+import {
+  getVehicles,
+  getVehicle,
+  postVehicle,
+  putVehicle,
+  deleteVehicle,
+  getVehicleValuesByVehicleId,
+} from "../controllers/Vehicle";
 
 const router = Router();
 
-router.get('/',       getVehicles);
-router.get('/:id',    getVehicle);
-router.post('/',      postVehicle);
-router.put('/:id',    putVehicle);
-router.delete('/:id', deleteVehicle);
+router.get("/", getVehicles);
+router.get("/vehicleValues/:id", getVehicleValuesByVehicleId);
+router.get("/:id", getVehicle);
+router.post("/", postVehicle);
+router.put("/:id", putVehicle);
+router.delete("/:id", deleteVehicle);
 
 module.exports = router;
